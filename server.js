bb2b6589b6a7e9b3722bf825c0da6d500a34f5fe
Juan D. Acosta <juandacorias@gmail.com>
2017-01-24 16:10:18 -0500

    Final version

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
-const http = require('http');
 const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-// Home
+// Home: Instructions for using the app
 app.get('/', (req, res) => {
   res.send('Please add at the end of the URL address "/api/whoami/"');
 });
 
-// Some String is passed in the URL
 app.get('/api/whoami', (req, res) => {
-	let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+	// Variable for string cleanup
 	let language = req.headers['accept-language'];
 	language = language.slice(0, 5);
+
 	let software = req.headers['user-agent'];
 	software = software.slice(software.indexOf('(') + 1, software.indexOf(')'));
 
   const info = {
-  	ipaddress: ip,
+  	ipaddress: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
   	language,
   	software,
   };
